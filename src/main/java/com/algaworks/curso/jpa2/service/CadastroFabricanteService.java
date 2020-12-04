241f3400b69a5fc658accf11a477b99c061f9ec8
claytonpereira <claytonpereira@STI-DES-RP2243.sad.infovia-mt>
2020-12-04 12:17:51 -0300

    saving initial commit .

---
+++ src/main/java/com/algaworks/curso/jpa2/service/CadastroFabricanteService.java
@@ -0,0 +1,27 @@
+package com.algaworks.curso.jpa2.service;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+
+import com.algaworks.curso.jpa2.dao.FabricanteDAO;
+import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
+
+public class CadastroFabricanteService implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private FabricanteDAO fabricanteDAO;
+  @Transactional	
+  public void salvar(Fabricante fabricante ) throws NegocioException {
+	  if(fabricante.getNome() == null || fabricante.getNome().trim().equals("")) {
+		  throw new NegocioException("Nome do fabricante não pode ser nulo !");
+	  }
+	  this.fabricanteDAO.salvar(fabricante);
+  }
+}
