3af8d9676589c1e27364c6f6d6554a302fef5c86
claytonpereira <claytonpereira@STI-DES-RP2243.sad.infovia-mt>
2020-12-07 10:25:11 -0300

    Including search and delete to project using JPA 2 and JSF as front-end.

--- src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
+++ src/main/java/com/algaworks/curso/jpa2/dao/FabricanteDAO.java
@@ -1,11 +1,14 @@
 package com.algaworks.curso.jpa2.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import com.algaworks.curso.jpa2.modelo.Fabricante;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
 
 public class FabricanteDAO implements Serializable {
 	/**
@@ -19,4 +22,16 @@ public class FabricanteDAO implements Serializable {
 	public void salvar(Fabricante fabricante) {
 	  em.persist(fabricante);
   }
+
+	@SuppressWarnings("unchecked")
+	public List<Fabricante> buscarTodos() {
+		return em.createQuery("from Fabricante").getResultList();
+	}
+    @Transactional
+	public void excluir(Fabricante fabricante) throws NegocioException {
+		Fabricante fabricantemp = em.find(Fabricante.class, fabricante.getCodigo());
+		em.remove(fabricantemp);
+		em.flush();
+		
+	}
 }
