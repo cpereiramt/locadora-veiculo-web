a69954a97a2fe6235a9186437fd3c2bef1d639ff
clayton pereira <cpereiramt@gmail.com>
2020-12-09 10:52:35 -0300

    include many to one JPA2 Example

--- src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
+++ src/main/java/com/algaworks/curso/jpa2/modelo/Fabricante.java
@@ -14,6 +14,29 @@ public class Fabricante {
 	public Long getCodigo() {
 		return codigo;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fabricante other = (Fabricante) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
 	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
